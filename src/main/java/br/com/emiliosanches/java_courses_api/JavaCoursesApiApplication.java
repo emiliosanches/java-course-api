a9677c783e7e5e82e9be27a1943cc3eddeac9a71
Emilio <emiliosneto13@gmail.com>
2024-01-31 08:41:28 -0300

    Add Courses Controller with hello-world route

--- src/main/java/br/com/emiliosanches/java_courses_api/JavaCoursesApiApplication.java
+++ src/main/java/br/com/emiliosanches/java_courses_api/JavaCoursesApiApplication.java
@@ -1,5 +1,7 @@
 package br.com.emiliosanches.java_courses_api;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaCoursesApiApplication {
 
 	public static void main(String[] args) {
+		Locale.setDefault(new Locale("en", "US"));
 		SpringApplication.run(JavaCoursesApiApplication.class, args);
 	}
 
---
+++ src/main/java/br/com/emiliosanches/java_courses_api/modules/courses/CoursesController.java
@@ -0,0 +1,16 @@
+package br.com.emiliosanches.java_courses_api.modules.courses;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("courses")
+public class CoursesController {
+  @GetMapping
+  public String helloWorld(@RequestParam(required = false) String name) {
+    return "Hello, " + (name != null ? name : "world!");
+  }
+
+}
