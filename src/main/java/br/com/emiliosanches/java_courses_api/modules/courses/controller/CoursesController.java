7b9f6c60f307775a21e66f31879015dbae777484
Emilio <emiliosneto13@gmail.com>
2024-01-31 09:03:00 -0300

    Define controller methods without implementation

--- src/main/java/br/com/emiliosanches/java_courses_api/modules/courses/controller/CoursesController.java
+++ src/main/java/br/com/emiliosanches/java_courses_api/modules/courses/controller/CoursesController.java
@@ -1,16 +1,39 @@
 package br.com.emiliosanches.java_courses_api.modules.courses.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("courses")
 public class CoursesController {
   @GetMapping
-  public String helloWorld(@RequestParam(required = false) String name) {
-    return "Hello, " + (name != null ? name : "world!");
+  public String listCourses() {
+    return "This method lists courses";
   }
 
+  @GetMapping("{id}")
+  public String getCourseById(@PathVariable String id) throws Exception {
+    return "This method shows the course " + id;
+  }
+
+  @PostMapping
+  public String createCourse() {
+    return "This method creates a course";
+  }
+
+  @PutMapping("{id}")
+  public String updateCourse(@PathVariable String id) {
+    return "This method updates the course " + id;
+  }
+
+  @DeleteMapping("{id}")
+  public String deleteCourse(@PathVariable String id) {
+    return "This method deletes the course " + id;
+  }
 }
