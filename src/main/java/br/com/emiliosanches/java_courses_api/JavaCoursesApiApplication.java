bebfbbec71d4d6d3e96dc26d9c42ef954af0421f
Emilio <emiliosneto13@gmail.com>
2024-01-31 08:28:53 -0300

    Initialize Spring Boot project

---
+++ src/main/java/br/com/emiliosanches/java_courses_api/JavaCoursesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emiliosanches.java_courses_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoursesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCoursesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emiliosanches/java_courses_api/JavaCoursesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emiliosanches.java_courses_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoursesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
